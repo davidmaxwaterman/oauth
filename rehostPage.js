4e821935e1d8006b7a00fbad9b1505b33e537dc7
Max Waterman <max.waterman@intel.com>
2014-10-21 09:36:48 +0100

    added 'component' property to message.

--- rehostPage.js
+++ rehostPage.js
@@ -1,23 +1,16 @@
 (function(){
 
+  // just relay messages between web app and background page
   chrome.runtime.onMessage.addListener(function(message,sender,sendResponse) {
-    console.log('CS:fromeventpage:',message);
-    if (message.type==='accesstoken') {
-      console.log('CS:received accesstoken');
-      document.dispatchEvent(
-        new CustomEvent('fromeventpage', {detail:message})
-      );
-    }
+    document.dispatchEvent(
+      new CustomEvent('fromeventpage', {detail:message})
+    );
     sendResponse('received, thanks');
   });
 
   // relay all messages to event page
   document.addEventListener('toeventpage',function(e) {
-    var message = {
-      type: e.detail.type,
-      data: e.detail.data
-    };
-    console.log('CS:toeventpage:',message);
+    var message = e.detail;
     chrome.runtime.sendMessage(message);
   });
 
